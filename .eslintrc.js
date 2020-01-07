fbd7deab0a468af7fdb97c706c6ecf7f86e082c5
Hans Jakob Emmel <hans.emmel@gmail.com>
2020-01-07 22:50:52 +1300

    Updating lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
     "extends": [
         "@the-unicorns/eslint-config-react"
-    ],
-    "rules": {
-        "no-unused-vars": ["warn", {"argsIgnorePattern": "^_"}],
-        "@typescript-eslint/no-unused-vars": ["warn", {"argsIgnorePattern": "^_"}]
-    }
+    ]
 };
